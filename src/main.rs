4e91774a69be910126a0865e8c74acc0fc716e59
rustmon <rustmon@lemon.local>
2023-10-21 09:14:09 +0200

    add more error handling, user can now use a file that already exist

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
-// -------------- Crypto ----------------
-
-// -------------- File ----------------
-//use std::fmt::write;
+use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::fs::OpenOptions;
 use std::io;
-// -------------- Magic Crypt ----------------
-use magic_crypt::{MagicCryptTrait, new_magic_crypt};
+use std::io::prelude::*;
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
 fn main() {
-    println!("Hi, please the name of the file");
+    let args: Vec<String> = env::args().collect();
 
-    let mut file_name = String::new();
-    io::stdin()
-        .read_line(&mut file_name)
-        .expect("Failed to read file name");
-    
-    println!("Please now enter a password in the file");
-    let mut user_entry = String::new();
-    io::stdin()
-        .read_line(&mut user_entry)
-        .expect("Failed to read input");
-
-    // delete the new line or escape character at the end of file name
-    file_name = file_name.trim().to_string();
-
-    if file_name.is_empty() {
-        eprintln!("You must choose a name for your file");
-        return;
+    if args.len() != 2 {
+        println!("[-] Usage : ./pmon <file> ")
     }
 
-    match create_file(&file_name) {
-        Ok(()) => println!("File {} created nicely", file_name),
-        Err(err) => eprintln!("Error : {}", err),
-    }
-    
-    let encrypted_content = encrypt_string(&user_entry);
-    match write_to_file(&file_name, &encrypted_content) {
-        Ok(()) => println!("You write something in the file !"),
-        Err(err) => eprintln!("Error : {}", err),
+    let file_path = &args[1];
+    println!("[+] Using file : {}", file_path);
+
+    let mut user_password = String::new();
+    if !file_path.is_empty() {
+        println!("[+] Enter a password in your file : ");
+        io::stdin()
+            .read_line(&mut user_password)
+            .expect("Failed to read input");
+
+        if user_password.trim().is_empty() {
+            eprintln!("[-] You must specify a password.");
+            return;
+        }
+
+        let result = write_to_file(file_path, &user_password);
+        match result {
+            Ok(()) => println!("[+] File created nicely"),
+            Err(err) => eprintln!("[-] Error when creating the file : {}", err),
+        } 
+
+    } else {
+        println!("[+] File path not specified. Creating one of your own : ");
+        let mut file_name = String::new();
+        io::stdin()
+            .read_line(&mut file_name)
+            .expect("Failed to read input");
+
+        match create_file(&file_name) {
+            Ok(()) => println!("We're in the match section !"),
+            Err(err) => eprintln!("Bug in the match : {}", err),
+        }
     }
 }
 
@@ -52,17 +57,31 @@ fn create_file(path: &str) -> io::Result<()> {
 }
 
 fn write_to_file(path: &str, content: &str) -> io::Result<()> {
-    // we need to open the file in write mode, File::create() let us do that
-    // also, note that File::open() is used to open a file in read mode
-    let mut file = File::create(path)?;
+    // open the file in read and write acces, and open the file if it exist
+    let mut file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(path)?;
+
+    // read the content of the file that we want to open
+    // existing_content will now contains the content of the file before writing in it
+    let mut existing_content = String::new();
+    file.read_to_string(&mut existing_content)?;
+
+    // push at the end of the file
+    existing_content.push_str(content);
+
+    // redirect the position for reading at the start of the file
+    file.seek(io::SeekFrom::Start(0))?;
     // we need to write the content as byte in the file because the write_all method
-    // take a slice of byte as argument   
-    file.write_all(content.as_bytes())?;
+    // take a slice of byte as argument
+    file.write_all(existing_content.as_bytes())?;
     Ok(())
 }
 
-fn encrypt_string(content: &str) -> String {
-    let mcrypt = new_magic_crypt!("magickey", 256);
-    let encrypted_string = mcrypt.encrypt_bytes_to_base64(content);
-    encrypted_string
-}
+// fn encrypt_string(content: &str) -> String {
+//     let mcrypt = new_magic_crypt!("magickey", 256);
+//     let encrypted_string = mcrypt.encrypt_bytes_to_base64(content);
+//     encrypted_string
+// }
